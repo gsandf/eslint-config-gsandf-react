8bc5d7ebc5760e123c10cbf0f4b9bec5559f4806
Blake Knight <bknight@gsandf.com>
2017-05-04 14:29:48 -0500

    :tada: initial commit

---
+++ eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: 'gsandf',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  plugins: ['react'],
+
+  rules: {
+    'react/jsx-no-bind': [
+      'warn',
+      {
+        allowArrowFunctions: true,
+        allowBind: false,
+        ignoreRefs: true
+      }
+    ],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-curly-spacing': ['error', 'never'],
+    'react/jsx-equals-spacing': ['error', 'never'],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-space-before-closing': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-unknown-property': 'error',
+    'react/no-unused-prop-types': 'error',
+    'react/prefer-es6-class': 'error',
+    'react/prop-types': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/self-closing-comp': 'error'
+  }
+}
